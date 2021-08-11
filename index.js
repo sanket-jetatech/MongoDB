58a408e20d61f03f683bab347cea07534eadfd92
sanket thotange <sanket@jetatech.in>
2021-08-11 20:33:18 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const { connectToServer } = require('./db');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.listen(5000);
 
 connectToServer();
