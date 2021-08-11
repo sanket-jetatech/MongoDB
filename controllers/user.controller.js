32c3acd1511a17b30dd86c90002f8353759b8c83
sanket thotange <sanketat@gmail.com>
2021-08-11 12:10:16 +0530

    error handling and delete new APIs

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,42 @@
 const { getDb } = require('../db');
 
 const getAllUsers = (req, res) => {
-    let db = getDb();
-    db.collection('Users').find().toArray((err, docs) => {
-        res.send(docs);
-    });
+    try {
+        let db = getDb();
+        db.collection('Users').find().toArray((err, docs) => {
+            res.send(docs);
+        });
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+const createUser = (req, res) => {
+    try {
+        let db = getDb();
+        let user = req.body;
+        db.collection('Users').insertOne(user, (err, result) => {
+            res.send("user added successfully!");
+        });
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+const deleteUser = (req, res) => {
+    try {
+        let db = getDb();
+        let userId = req.params.id;
+        db.collection('Users').deleteOne({ "_id": userId }, (err, result) => {
+            res.send("user deleted successfully!");
+        });
+    } catch (err) {
+        res.send(err);
+    }
 }
 
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    createUser,
+    deleteUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/user.controller');
 const router = express.Router();
 
 router.get("/", userController.getAllUsers);
+router.post("/", userController.createUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
